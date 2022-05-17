93896b5b9bbdf62d992bf423010a736bce51b1d6
Victor Magos <victormagos@pm.me>
2022-05-16 23:50:35 -0500

    Amod-Magos

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = { clicked: false };
+
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">
+          React <i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
